d4f76e5307cf0f9ada3fe1be715c491b5667617e
Jack Branch <jackbranch@Jacks-MacBook-Pro.local>
2018-07-10 12:24:54 +0100

    rspec test e

--- Program.rb
+++ Program.rb
@@ -6,6 +6,15 @@ end
 
 def test_2(x)
 
-  return x + 1 
+  return x + 1
 
 end
+
+# test for int to array
+def test_3(x)
+  
+  return x.to_s.split('')
+
+end
+
+test_3(1234567)
--- spec/Tests_spec.rb
+++ spec/Tests_spec.rb
@@ -16,3 +16,16 @@ describe  "test_2" do
     expect(value).to eq(x + 1)
   end
 end
+
+# more complex test using fixed input instances
+describe "test_3" do
+  before(:each) do # creating our input instance
+    @input = 1234567
+  end
+  it "returns correct output" do
+    expect(test_3(@input)).to eq ["1", "2", "3", "4", "5", "6", "7"]
+  end
+  it "is the correct length" do
+    expect(test_3(@input).length).to eq 7
+  end
+end
